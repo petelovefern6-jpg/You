8c5d8ca9d2594b0a93fbaa4c08c56cb08e86dcbf
petelovefern6-jpg <petelovefern6@gmail.com>
2026-07-07 14:45:38 +0700

    Add initial Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+  output: 'standalone',
+  trailingSlash: true,
+};
+
+module.exports = nextConfig;
